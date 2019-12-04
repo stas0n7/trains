8526259d68f82d0cfffc85174c281ffc81161e8e
stas0n7 <r14war@gmail.com>
2019-12-05 01:01:25 +0200

    complete STI

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,6 +1,8 @@
 class Carriage < ApplicationRecord
   validates :number, presence: true
   belongs_to :train
-  scope :coupe, ->   { where(type: 'CoupeCarriage') }
+  scope :coupe,   -> { where(type: 'CoupeCarriage') }
   scope :economy, -> { where(type: 'EconomyCarriage') }
+  scope :sv,      -> { where(type: 'SvCarriage') }
+  scope :seated,  -> { where(type: 'SeatedCarriage') }
 end
