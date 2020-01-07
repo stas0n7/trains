0fc8853eb66d7953e4af128cf07e12a27f1bc843
stas0n7 <r14war@gmail.com>
2020-01-07 18:17:35 +0200

    Complete lesson17

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -7,6 +7,6 @@ class Admin::BaseController < ApplicationController
   protected
 
   def check_admin
-    redirect_to root_path, alert: 'У вас нет прав на просмотр этой страницы' unless current_user.admin?
+    redirect_to root_path, alert: 'You have no access to this page' unless current_user.admin?
   end
 end
