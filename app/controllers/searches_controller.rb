808648b2100aa67b6d12847f23ca2a726b356ade
stas0n7 <r14war@gmail.com>
2020-01-26 14:11:20 +0200

    fixed search bug

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,5 +5,6 @@ class SearchesController < ApplicationController
 
   def show
     @routes = RailwayStation.find(params[:start_id]).routes & RailwayStation.find(params[:end_id]).routes
+    @routes.each { |route| @trains ||= 1 if route.trains.any? }
   end
 end
